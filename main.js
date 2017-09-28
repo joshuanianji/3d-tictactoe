9fa0589a624c958ffd6436cb3e665d11e651e002
Joshua <j.ji@share.epsb.ca>
2017-09-28 11:10:13 -0600

    vital change
    -steven 2017

--- main.js
+++ main.js
@@ -3,26 +3,27 @@ console.log('Hi!');
 
 //so this imports three.js modules and creates a scene.
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 //creates a box
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
 //creates a loop that animates the cube
 function animateCube() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     cube.rotation.x += 0.1;
     cube.rotation.y += 0.1;
-	renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 animateCube();
-
