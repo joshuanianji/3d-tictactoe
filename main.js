3478bb3a8e2cb1c665037d25fc7484dd31941e07
Steven Tang <TangSteven@users.noreply.github.com>
2017-09-29 10:42:31 -0600

    ?

--- main.js
+++ main.js
@@ -6,9 +6,13 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio(2);
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+var light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(1, 1, 1).normalize();
+scene.add(light);
 
 //creates a box
 var distance = 3;
@@ -22,7 +26,7 @@ for (var h = 0; h < 3; h++) {
     for (var j = 0; j < 3; j++) {
       var cube = new THREE.Mesh(geometry, material);
       cube.position.y = (distance * h) - 3;
-      cube.position.x = (distance * i) - 3;
+      cube.position.x = (distance * i) - 6;
       cube.position.z = (distance * j);
       cubes.push(cube);
       scene.add(cube);
@@ -47,25 +51,33 @@ function render() {
 }
 render();
 
-window.addEventListener('mousedown', () => {
+var intersected;
+window.addEventListener('mousedown', e => {
   var raycaster = new THREE.Raycaster();
   var mouse = new THREE.Vector2();
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
   // update the picking ray with the camera and mouse position
 
   raycaster.setFromCamera(mouse, camera);
   // calculate objects intersecting the picking ray
   var intersects = raycaster.intersectObjects(scene.children);
-  for (var i = 0; i < intersects.length; i++) {
-    if (cube.material.color.equals({
+  console.log(intersects);
+  if (intersects.length > 0) {
+    if (intersects[0].object.material.color.equals({
         r: 1,
         g: 1,
         b: 1
       })) {
-      intersects[i].object.material.color.set('#ff0000');
+      intersects[0].object.material.color.set('#ff0000');
     } else {
-      intersects[i].object.material.color.set('#fff');
+      intersects[0].object.material.color.set('#fff');
     }
   }
+}, false);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }, false);
