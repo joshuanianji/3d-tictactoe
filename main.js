1cec7cb733455bd592d4ed9208ac13d69ab45afc
Steven Tang <TangSteven@users.noreply.github.com>
2017-09-28 21:56:56 -0600

    Set Pixel Ratio!

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(2);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -23,8 +24,8 @@ function render() {
   window.requestAnimationFrame(render);
 
   //animateCube
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
