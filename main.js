2d25547ef7c2a66e02297ae96692b55984c8a65b
Steven Tang <TangSteven@users.noreply.github.com>
2017-09-28 18:08:11 -0600

    Click to change color!

--- main.js
+++ main.js
@@ -12,38 +12,40 @@ document.body.appendChild(renderer.domElement);
 //creates a box
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({
-  color: '#b5169c'
+  color: '#fff'
 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-
-function onMouseMove(event) {
-  // calculate mouse position in normalized device coordinates
-  // (-1 to +1) for both components
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-}
-
 function render() {
   window.requestAnimationFrame(render);
-  // update the picking ray with the camera and mouse position
-  raycaster.setFromCamera(mouse, camera);
-  // calculate objects intersecting the picking ray
-  var intersects = raycaster.intersectObjects(scene.children);
-  for (var i = 0; i < intersects.length; i++) {
-    intersects[i].object.material.color.set(0xff0000);
-  }
+
   //animateCube
   cube.rotation.x += 0.1;
   cube.rotation.y += 0.1;
 
   renderer.render(scene, camera);
 }
-
-window.addEventListener('mousemove', onMouseMove, false);
 render();
+
+window.addEventListener('mousedown', () => {
+  var raycaster = new THREE.Raycaster();
+  var mouse = new THREE.Vector2();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  // update the picking ray with the camera and mouse position
+
+  raycaster.setFromCamera(mouse, camera);
+  // calculate objects intersecting the picking ray
+  var intersects = raycaster.intersectObjects(scene.children);
+  for (var i = 0; i < intersects.length; i++) {
+    if (cube.material.color.equals({r: 1, g: 1, b: 1})) {
+      console.log(intersects[i].object.material.color);
+      intersects[i].object.material.color.set('#ff0000');
+    } else {
+      intersects[i].object.material.color.set('#fff');
+    }
+  }
+}, false);
