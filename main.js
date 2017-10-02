9872e38bfb666b9f02b6b0449645a2bab9a76ab4
Joshua <j.ji@share.epsb.ca>
2017-10-02 10:31:16 -0600

    cube stuff lmao

--- main.js
+++ main.js
@@ -11,18 +11,39 @@ camera.lookAt(new THREE.Vector3(0, 0, 0));
 //important render stuff
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio(2);
+renderer.setPixelRatio(3);
 document.body.appendChild( renderer.domElement );
 
 //create a blue LineBasicMaterial
-var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+var material = new THREE.LineBasicMaterial({ color: 0x0152FF });
 var geometry = new THREE.Geometry();
 geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
 geometry.vertices.push(new THREE.Vector3(0, 10, 0));
 geometry.vertices.push(new THREE.Vector3(10, 0, 0));
 var line = new THREE.Line(geometry, material);
 
-scene.add(line);
+//creating a torus
+var geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+var material = new THREE.MeshBasicMaterial( { color: 0x0152FF } );
+var torus = new THREE.Mesh( geometry, material );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( {color: 0x0152FF} );
+var cube = new THREE.Mesh( geometry, material );
+
+// scene.add stuff
+// scene.add(line);
+scene.add( torus );
+// scene.add( cube );
 renderer.render(scene, camera);
 
-// animate();
+function render() {
+    window.requestAnimationFrame(render);
+  
+    //animateTorus
+      torus.rotation.x += 0.01;
+      torus.rotation.y += 0.01;
+  
+    renderer.render(scene, camera);
+  }
+render();
